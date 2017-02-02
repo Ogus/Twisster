b79ec1cc8c0b0c0f28f2d95f265084d31211252c
Ogus <ogustin.gagnon@gmail.com>
2017-02-02 11:57:50 +0100

    Ajout de la génération du tweet

--- js/maps.js
+++ js/maps.js
@@ -12,7 +12,10 @@ window.onload = function(){
     var prev_accept = document.getElementById("preview_accept");
 
   var map_layer, tile_layer, marker,polyline;
-  var ajax, json_result, position={lat: 0, lng: 0}, zoom_level = 3;
+  var ajax_position, ajax_geoname, json_resul;
+  var position={lat: 999, lng: 0}, old_position={lat: 0, lng: 0}, zoom_level = 3;
+
+  var loaded = false;
 
   //icone personnalisé
   var station_icon = L.icon({
@@ -41,25 +44,25 @@ window.onload = function(){
     tweet_button.addEventListener('click', generate_tweet, false);    //bouton pour lancer la génération du tweet
 
     prev_dismiss.addEventListener('click', function(){    //bouton pour annuler le tweet
+    window.setTimeout(function(){ document.getElementById("overlay").style.top = "100%"; }, 800);
       document.getElementById("preview").style.top = "150%";
-      document.getElementById("overlay").style.top = "100%";
     })
 
 
     map_layer = L.map('map').setView([48.853, 2.345], zoom_level);
+    // map_layer.remove();
     marker = L.marker([], {icon: station_icon});
-    polyline = L.polyline([], {color: 'green', smoothFactor: 3.0}).addTo(map_layer);
+    marker.bindPopup("Position de l'ISS");
+    polyline = L.polyline([], {color: 'green', smoothFactor: 3.0});
 
     tile_layer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
       maxZoom:18,
-    	minZoom:3,
+    	minZoom:1,
     	id:'mapbox.streets',   //mapbox.mapbox-streets-v7  //mapbox.mapbox-terrain-v2  //mapbox.satellite
     	accessToken: 'pk.eyJ1IjoiY2FzdG9yYm90IiwiYSI6ImNpaWkweWQ5ajAwaHV1NmtueHV1MHowcHgifQ.2l9JrRXro_ve9S_pMdTB0Q'
     	});
     tile_layer.addTo(map_layer);
-
-    marker.bindPopup("Position de l'ISS");
 
     set_zoom();
     update();
@@ -89,55 +92,142 @@ window.onload = function(){
       }
     }
     map_layer.setZoom(zoom_level);
+
+    if(follow_option.checked == true){ map_layer.panTo(position); }
   }
 
   //boucle de requêtes sur la position de l'ISS
   function update(){
-    ajax = new XMLHttpRequest();
-    ajax.open("GET","http://api.open-notify.org/iss-now.json",true);
-    ajax.onreadystatechange = update_map;
-    ajax.send();
+    ajax_position = new XMLHttpRequest();
+    ajax_position.open("GET","http://api.open-notify.org/iss-now.json",true);
 
-    window.setTimeout(update, 5000);    //màj toute les 5 secondes
+    ajax_position.onreadystatechange = function(){
+      if(ajax_position.readyState == 4 && ajax_position.status == 200){
+        json_result = JSON.parse(ajax_position.responseText);    //on récupère la position au format json
+
+        old_position.lat = position.lat;
+        old_position.lng = position.lng;
+        position.lat = json_result.iss_position.latitude;
+        position.lng = json_result.iss_position.longitude;
+        update_map();
+      }
+    };
+    ajax_position.send();
+
+    window.setTimeout(update, 3000);    //màj toute les 3 secondes
   }
 
   //màj de la carte à chaque nouvelle position de l'ISS
   function update_map(){
-    if(ajax.readyState == 4 && ajax.status == 200){
-      json_result = JSON.parse(ajax.responseText);    //on récupère la position au format json
-      position.lat = json_result.iss_position.latitude;
-      position.lng = json_result.iss_position.longitude;
+    marker.setLatLng(position).addTo(map_layer);    //on change la positon du marker
 
-      marker.setLatLng(position).addTo(map_layer);    //on change la positon du marker
-      polyline.addLatLng(position);                   //on ajoute le point à la polyligne
-      if(follow_option.checked == true){ map_layer.panTo(position); }     //on change le cadrage de la carte pour suivre l'ISS
+    if(follow_option.checked == true){ map_layer.panTo(position); }     //on change le cadrage de la carte pour suivre l'ISS
 
-      lat_text.innerHTML = "<span>Latitude: </span>"+String(position.lat);
-      long_text.innerHTML = "<span>Longitude: </span>"+String(position.lng);
+    if( !(Math.sign(old_position) != Math.sign(position) && Math.abs(position) > 150) && old_position.lat != 999){
+      L.polyline([old_position,position], {color: 'green'}).addTo(map_layer);
     }
+
+    lat_text.innerHTML = "<span>Latitude: </span>"+String(position.lat);
+    long_text.innerHTML = "<span>Longitude: </span>"+String(position.lng);
   }
+
 
   //génère un faux tweet lors du clique sur le bouton 'tweet comme pesquet'
   function generate_tweet(){
     document.getElementById("overlay").style.top = "0";
+    document.getElementsByClassName("loading")[0].style.display = "flex";
+    load_image();
+    load_geoname();
+  }
+
+  //charge l'image satellite
+  function load_image(){
     zoom_level = map_layer.getZoom();
 
-    var bearing = Math.floor(360*Math.random());
+    var bearing = 360*Math.random();
     var url = "https://api.mapbox.com/styles/v1/castorbot/ciylak035004z2ro27w5r5p5s/static/"    //url de l'image statique
                 +String(position.lng)+","    //position
                 +String(position.lat)+","
                 +String(zoom_level)+","   //zoom
-                +String(bearing)//+","      //bearing
-                // +String(0.0)          //pitch
-                +"/400x400?access_token=pk.eyJ1IjoiY2FzdG9yYm90IiwiYSI6ImNpaWkweWQ5ajAwaHV1NmtueHV1MHowcHgifQ.2l9JrRXro_ve9S_pMdTB0Q";
+                +String(bearing)     //bearing
+                // +","+String(0.0)          //pitch
+                +"/400x400"
+                +"?access_token=pk.eyJ1IjoiY2FzdG9yYm90IiwiYSI6ImNpaWkweWQ5ajAwaHV1NmtueHV1MHowcHgifQ.2l9JrRXro_ve9S_pMdTB0Q&attribution=false&logo=false";
 
     var img = document.getElementById("preview_img");
     img.src = url;
     img.addEventListener('load', function(){      //on attend que l'image soit chargé avant de montrer le faux tweet
-      document.getElementById("preview").style.top = "50%";
+      if(loaded){
+        document.getElementById("preview").style.top = "50%";
+        document.getElementsByClassName("loading")[0].style.display = "none";
+        loaded = false;
+      }
+      else{ loaded = true; }
     }, false);
   }
 
+  //charge la localisation la plus proche
+  function load_geoname(){
+    url = "http://api.geonames.org/extendedFindNearby?"
+          +"lat="+String(position.lat)+"&"
+          +"lng="+String(position.lng)+"&username=cocyte";
+
+    ajax_geoname = new XMLHttpRequest();
+    ajax_geoname.open("GET",url,true);
+
+    ajax_geoname.onreadystatechange = function(){
+      if(ajax_geoname.readyState == 4 && ajax_geoname.status == 200){
+        var xml = ajax_geoname.responseXML;
+        var json_text = JSON.stringify(xml_to_json(xml));   //conversion du xml en json
+        // console.log(json_text);
+        var geonames = JSON.parse(json_text).geonames;
+        var location, hashtag;
+
+        //définis le type de localisation et le type de texte du tweet
+        if(geonames.address != undefined){
+          location = geonames.address.adminName2;
+          hashtag = "#"+geonames.address.adminName1;
+        }
+        else if(geonames.ocean != undefined){
+          location = "the "+geonames.ocean.name;
+          hashtag = "#BluePlanet"
+        }
+        else if(geonames.geoname.name != null){
+          location = geonames.geoname.name;
+          hashtag = "#"+geonames.geoname.countryName;
+        }
+        else{
+          location = geonames.countryName;
+          hashtag = "";
+        }
+        document.getElementById("preview_txt").innerHTML = "Nice view above "+location+" ! <span class='hashtag'>"+hashtag+" #ISS</span>";
+
+        if(loaded){
+          document.getElementById("preview").style.top = "50%";
+          document.getElementsByClassName("loading")[0].style.display = "none";
+          loaded = false;
+        }
+        else{ loaded = true; }
+      }
+    };
+
+    ajax_geoname.send();
+  }
+
+
+  //function recursive pour convertir un fichier xml en json
+  function xml_to_json(xml){
+    var obj = {};
+
+    if(xml.hasChildNodes()){
+      for(var i=0; i<xml.childNodes.length; i++){
+        var item = xml.childNodes[i];
+          if(item.nodeType == 1){ obj[item.nodeName] = xml_to_json(item); }
+          else if(item.nodeType == 3 && item.nodeValue != "\n"){ obj = item.nodeValue; }
+  		}
+    }
+    return obj;
+  }
 
 
 }
